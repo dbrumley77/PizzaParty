38e8e0a65bed1800b9a7c47835f4b813112d70bb
Douglas <douglas-brumley@hotmail.com>
2024-07-22 15:30:42 -0400

    Images added, text color/layout changed

--- obj/Debug/net8.0/PizzaParty.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzaParty.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaParty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd1aa63bbdfbaa18541dc04870ef321f94253ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03af38c6df56c56dbb456e5b914fbc7d03d7c886")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaParty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaParty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
